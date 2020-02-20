902807316023f024ced0468f626afec0ba5ea4d0
Farhan Bashir <fb@linkmedia.com>
2020-02-20 15:40:13 +0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodosComponent } from './todos/todos.component';
+import { TodosComponent } from './components/todos/todos.component';
 import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TodosComponent, TodoComponent],
+  declarations: [ 
+    AppComponent, 
+    TodosComponent, 
+    TodoComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
