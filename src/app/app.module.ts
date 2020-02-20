74e122a9707db37b69f2675c8c7321fe80667674
Farhan Bashir <fb@linkmedia.com>
2020-02-20 14:40:13 +0400

    update pakage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, TodosComponent, TodoComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
